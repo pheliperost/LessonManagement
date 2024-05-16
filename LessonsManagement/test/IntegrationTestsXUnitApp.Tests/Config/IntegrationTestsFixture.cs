468e582ed29594be0003cd5f8dcc27b2d00f81db
Phelipe Rost <phelipe_rost@hotmail.com>
2024-05-16 18:25:16 -0300

    Configurações iniciais para testes de integração.

---
+++ LessonsManagement/test/IntegrationTestsXUnitApp.Tests/Config/IntegrationTestsFixture.cs
@@ -0,0 +1,33 @@
+﻿using IntegrationTestsXUnitApp.Tests.Config;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace IntegrationTestsXUnitApp.Tests.Config
+{
+    [CollectionDefinition(nameof(IntegrationWebTestsFixtureCollection))]
+
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly LessonsManagementAppFactory<TStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixture()
+        {
+            var clientOptions = new WebApplicationFactoryClientOptions {
+
+            };
+
+            Factory = new LessonsManagementAppFactory<TStartup>();
+            Client = Factory.CreateClient();
+        }
+        public void Dispose()
+        {
+            Client.Dispose();
+            Factory.Dispose();
+        }
+    }
+}
---
+++ LessonsManagement/test/IntegrationTestsXUnitApp.Tests/Config/IntegrationWebTestsFixtureCollection.cs
@@ -0,0 +1,9 @@
+﻿using LessonsManagement.App;
+using Xunit;
+
+namespace IntegrationTestsXUnitApp.Tests.Config
+{
+    public class IntegrationWebTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>>
+    {
+    }
+}
